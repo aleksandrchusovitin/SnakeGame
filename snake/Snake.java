53cf8b8ff098c65646b849bbe3460d26b282471d
nerealist <38918351+nerealist@users.noreply.github.com>
2019-09-03 19:04:21 +0700

    Update Snake.java

--- snake/Snake.java
+++ snake/Snake.java
@@ -1,6 +1,7 @@
 package com.javarush.games.snake;
 
 import java.util.*;
+
 import com.javarush.engine.cell.*;
 
 public class Snake {
@@ -9,42 +10,43 @@ public class Snake {
     private List<GameObject> snakeParts = new ArrayList<>();
     public boolean isAlive = true;
     private Direction direction = Direction.LEFT;
-    
+
     Snake(int x, int y) {
         GameObject snake1 = new GameObject(x, y);
         GameObject snake2 = new GameObject(x + 1, y);
         GameObject snake3 = new GameObject(x + 2, y);
-        
+
         snakeParts.add(snake1);
         snakeParts.add(snake2);
         snakeParts.add(snake3);
     }
-    
+
     public void draw(Game game) {
         game.setCellValueEx(snakeParts.get(0).x, snakeParts.get(0).y, Color.NONE, HEAD_SIGN, (isAlive ? Color.BLACK : Color.RED), 75);
-        game.setCellValueEx(snakeParts.get(1).x, snakeParts.get(1).y, Color.NONE, BODY_SIGN, (isAlive ? Color.BLACK : Color.RED), 75);
-        game.setCellValueEx(snakeParts.get(2).x, snakeParts.get(2).y, Color.NONE, BODY_SIGN, (isAlive ? Color.BLACK : Color.RED), 75);
+        for (int i = 1; i < snakeParts.size(); i++) {
+            game.setCellValueEx(snakeParts.get(i).x, snakeParts.get(i).y, Color.NONE, BODY_SIGN, (isAlive ? Color.BLACK : Color.RED), 75);
+        }
     }
-    
+
     public void setDirection(Direction direction) {
         if ((direction == Direction.LEFT && (snakeParts.get(0).y == snakeParts.get(1).y)) ||
-        (direction == Direction.RIGHT && (snakeParts.get(0).y == snakeParts.get(1).y)) ||
-        (direction == Direction.UP && (snakeParts.get(0).x == snakeParts.get(1).x)) ||
-        (direction == Direction.DOWN && (snakeParts.get(0).x == snakeParts.get(1).x))) {
+                (direction == Direction.RIGHT && (snakeParts.get(0).y == snakeParts.get(1).y)) ||
+                (direction == Direction.UP && (snakeParts.get(0).x == snakeParts.get(1).x)) ||
+                (direction == Direction.DOWN && (snakeParts.get(0).x == snakeParts.get(1).x))) {
             return;
         }
-        
-        if ((direction == Direction.LEFT && this.direction == Direction.RIGHT) || 
-        (direction == Direction.RIGHT && this.direction == Direction.LEFT) || 
-        (direction == Direction.UP && this.direction == Direction.DOWN) ||
-        (direction == Direction.DOWN && this.direction == Direction.UP)) {
+
+        if ((direction == Direction.LEFT && this.direction == Direction.RIGHT) ||
+                (direction == Direction.RIGHT && this.direction == Direction.LEFT) ||
+                (direction == Direction.UP && this.direction == Direction.DOWN) ||
+                (direction == Direction.DOWN && this.direction == Direction.UP)) {
             return;
         } else {
-            this.direction = direction;    
+            this.direction = direction;
         }
-        
+
     }
-    
+
     public void move(Apple apple) {
         GameObject newHead = createNewHead();
         int x = newHead.x;
@@ -62,7 +64,7 @@ public class Snake {
             isAlive = false;
         }
     }
-    
+
     public GameObject createNewHead() {
         int x = 0;
         int y = 0;
@@ -86,27 +88,24 @@ public class Snake {
             default:
                 break;
         }
-        
-        GameObject snakeN = new GameObject(x, y);
-        
-        
-        return snakeN;
+
+        return new GameObject(x, y);
     }
-    
+
     public void removeTail() {
         snakeParts.remove(snakeParts.size() - 1);
     }
-    
+
     public boolean checkCollision(GameObject go) {
-         for (GameObject obj: snakeParts) {
-             if (obj.x == go.x && obj.y == go.y) {
-                 return true;
-             }
-         }
-         return false;
+        for (GameObject obj : snakeParts) {
+            if (obj.x == go.x && obj.y == go.y) {
+                return true;
+            }
+        }
+        return false;
     }
-     
+
     public int getLength() {
         return snakeParts.size();
-    } 
+    }
 }
