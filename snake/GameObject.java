0ebed3d99d1687012e520344dde6151923b8ec2b
nerealist <38918351+nerealist@users.noreply.github.com>
2019-09-03 19:03:31 +0700

    Update GameObject.java

--- snake/GameObject.java
+++ snake/GameObject.java
@@ -1,10 +1,9 @@
 package com.javarush.games.snake;
 
-
 public class GameObject {
     public int x;
     public int y;
-    
+
     GameObject(int x, int y) {
         this.x = x;
         this.y = y;
